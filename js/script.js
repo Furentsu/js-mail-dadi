aa4c609828ca15d86a5fe986f9b0280c0344017b
Furentsu <eugeniofranzoso@gmail.com>
2021-08-06 16:41:12 +0200

    Completed second exercise

--- js/script.js
+++ js/script.js
@@ -1,23 +1,41 @@
 //  MAIL
 
-let mailingList = ["penny01@yahoo.it", "gionni90@gmail.com", "tommy@virgilio.it", "gerry.09@gmail.com", "larry@yahoo.it"];
+// let mailingList = ["penny01@yahoo.it", "gionni90@gmail.com", "tommy@virgilio.it", "gerry.09@gmail.com", "larry@yahoo.it"];
 
-let input = prompt("Inserisci la tua email");
+// let input = prompt("Inserisci la tua email");
 
-let login;
+// let login;
 
-for (let i = 0; i < mailingList.length; i++) {
-    if (mailingList.includes(input)) {
-        login = "La tua email risulta essere nel nostro database. Benvenuto!";
-    } else {
-        login = "Ti piacerebbe!!!"
-    }
-}
+// for (let i = 0; i < mailingList.length; i++) {
+//     if (mailingList.includes(input)) {
+//         login = "La tua email risulta essere nel nostro database. Benvenuto!";
+//     } else {
+//         login = "Ti piacerebbe!!!"
+//     }
+// }
 
-document.getElementById("login").innerHTML = login;
+// document.getElementById("login").innerHTML = login;
 
 
 // DADI
+
+let player = Math.floor(Math.random() * 6) + 1;
+console.log(player);
+
+let computer = Math.floor(Math.random() * 6) + 1;
+console.log(computer);
+
+let result;
+
+if (player > computer) {
+    result = "Complimenti, hai vinto!!";
+} else if (player < computer) {
+    result = "Prova ancora..."; 
+} else {
+    result = "Pareggio!";
+}
+
+document.getElementById("result").innerHTML = result;
 
 
 
